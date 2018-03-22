3fa8638087be0e5e2060978f607c5dcfac8035e1
Marco Moroni <marcomoroni394@gmail.com>
2018-03-22 11:52:20 +0000

    Steering

--- practical_7_steering/scenes/scene_steering.cpp
+++ practical_7_steering/scenes/scene_steering.cpp
@@ -1,6 +1,7 @@
 #include "scene_steering.h"
 #include "../components/cmp_sprite.h"
 #include "../components/cmp_basic_movement.h"
+#include "../components/cmp_ai_steering.h"
 #include <LevelSystem.h>
 #include <random>
 #include <chrono>
@@ -17,6 +18,22 @@ void SteeringScene::Load()
 	s->setShape<CircleShape>(10.0f);
 	s->getShape().setFillColor(Color::Red);
 	player->addComponent<BasicMovementComponent>();
+
+	// Setup C++ random number generation
+	random_device dev;
+	default_random_engine engine(dev());
+	uniform_real_distribution<float> x_dist(0.0f, Engine::GetWindow().getSize().x);
+	uniform_real_distribution<float> y_dist(0.0f, Engine::GetWindow().getSize().y);
+	
+	for (size_t n = 0; n < 100; ++n)
+	{
+		auto enemy = makeEntity();
+		enemy->setPosition(Vector2f(x_dist(engine), y_dist(engine)));
+		auto s = enemy->addComponent<ShapeComponent>();
+		s->setShape<RectangleShape>(Vector2f(10.0f, 10.0f));
+		s->getShape().setFillColor(Color::Blue);
+		enemy->addComponent<SteeringComponent>(player.get());
+	}
 }
 
 void SteeringScene::UnLoad() { Scene::UnLoad(); }
